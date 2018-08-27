286b204b0905cc5cbec75dca85cf66486d2f4ec2
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-27 20:37:34 +0530

    Solution.java

---
+++ Assignment-3/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args) {
+		double fahrenheit;
+		Scanner scan = new Scanner(System.in);
+		double degreesCelsius = scan.nextDouble();
+		fahrenheit = degreesCelsius*(9.0/5.0)+32;
+		System.out.println(fahrenheit);
+	}
+	/*
+	Need to fill the calculateFromDegreesToFarenheit function and print the output
+	of fahrenheit.
+	*/
+}
