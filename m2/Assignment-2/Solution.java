27a5de81b448da9d02c3b242085330018d3b4b5e
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-29 20:45:30 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m2/Assignment-2/Solution.java
+++ m2/Assignment-2/Solution.java
@@ -10,7 +10,7 @@ public class Solution {
      * @details [long description]
      * @param args [description]
      **/
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
         int b = scan.nextInt();
