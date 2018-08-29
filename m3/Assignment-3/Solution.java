659c8365d28d283598b4d22870cdab7ed2b9844f
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-29 23:57:25 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ m3/Assignment-3/Solution.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+/**
+ *this is import
+ */
+
+final class Solution {
+    /**
+     * this is a class
+     */
+    private Solution() { }
+    /**
+     * @param args [description]
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+        gcd(n, m);
+        System.out.println(gcd(n, m));
+    }
+
+    /**
+     * @param n num
+     * @param m num
+     * @return [description]
+     */
+
+    public static int gcd(final int n, final int m) {
+        if  (m == 0) {
+            return n;
+        } else {
+        return gcd(m, (n % m));
+    }
+
+    }
+}
+
