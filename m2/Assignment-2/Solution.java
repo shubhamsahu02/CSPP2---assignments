075a535c7ac66c286967abf1c374c5eebba6a62f
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-29 20:37:12 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m2/Assignment-2/Solution.java
+++ m2/Assignment-2/Solution.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
 
 public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
-	}
-	/*
-	Need to write the rootsOfQuadraticEquation function and print the output.
-	*/
-public static void rootsOfQuadraticEquation(int a, int b, int c) {
-	int t = 4;
-	double result = ((b * b) - t * a * c);
-	double root1 = (-b + Math.sqrt(result)) / (2 * a);
-	double root2 = (-b - Math.sqrt(result)) / (2 * a);
+    /*
+    Do not modify this main function.
+    */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        rootsOfQuadraticEquation(a, b, c);
+    }
+    /*
+    Need to write the rootsOfQuadraticEquation function and print the output.
+    */
+public static void rootsOfQuadraticEquation(final int a, final int b, final int c) {
+    int t = 4;
+    double result = ((b * b) - t * a * c);
+    double root1 = (-b + Math.sqrt(result)) / (2 * a);
+    double root2 = (-b - Math.sqrt(result)) / (2 * a);
     System.out.println(root1 + " " + root2);
 }
 }
