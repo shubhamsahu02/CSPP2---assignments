efd87709c5f88a57e5990787ec5c6034f71ab660
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-30 19:48:17 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-1/Solution.java
+++ m4/Assignment-1/Solution.java
@@ -1,39 +1,39 @@
 
 import java.util.Scanner;
-public class Solution {
+final class Solution {
 /*
-	Fill this main function to print maximum of given array
-	*/
-	/**
-	 * @brief
-	 * @details
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-		int[] arr;
-		arr = new int[n];
-		for (int i = 0; i < n; i++) {
-			arr[i] = s.nextInt();
-		}
-		int temp = 0;
-		for(int i = 0; i < n; i++) {
-			if (i < n - 1) {
-			if (arr[i] > arr[i + 1]) {
-				if (arr[i] > temp) {
-					temp = arr[i];
-				}
-			}
-		}
-		if (arr[n - 1] > temp) {
-			temp = arr[n - 1];
-		} else {
-			temp = temp;
-		}
-	}
-	System.out.println(temp);
+    Fill this main function to print maximum of given array
+    */
+    /**
+     * @brief
+     * @details
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] arr;
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+        }
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+         if (i < n - 1) {
+            if (arr[i] > arr[i + 1]) {
+                if (arr[i] > temp) {
+                    temp = arr[i];
+                }
+            }
+        }
+        if (arr[n - 1] > temp) {
+            temp = arr[n - 1];
+        } else {
+            temp = temp;
+        }
+    }
+    System.out.println(temp);
 
-	}
+    }
 }
