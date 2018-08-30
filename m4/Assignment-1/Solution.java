6e47a116ce1198084e6cbdb866c1b8a6dfc45074
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-30 19:40:48 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-1/Solution.java
+++ m4/Assignment-1/Solution.java
@@ -1,30 +1,29 @@
 
-import java.util.*;
-public class Solution
-{/*
+import java.util.Scanner;
+public class Solution {
+/*
 	Fill this main function to print maximum of given array
 	*/
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n = s.nextInt();
 		int[] arr;
 		arr = new int[n];
 		for (int i = 0; i < n; i++) {
-			arr[i] = sc.nextInt();
+			arr[i] = s.nextInt();
 		}
 		int temp = 0;
 		for(int i = 0; i < n; i++) {
-			if (i < n-1) {
-			if(arr[i] > arr[i+1]) {
+			if (i < n - 1) {
+			if(arr[i] > arr[i + 1]) {
 				if(arr[i] > temp) {
 					temp = arr[i];
 				}
 			}
 		}
-		if (arr[n-1] > temp) {
+		if (arr[n - 1] > temp) {
 			temp = arr[n-1];
-		}else {
+		} else {
 			temp = temp;
 		}
 	}
