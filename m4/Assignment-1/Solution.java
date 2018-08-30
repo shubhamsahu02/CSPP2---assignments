668f68f33acce7e95eaf957807f0851ce47d11a2
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-30 20:02:22 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-1/Solution.java
+++ m4/Assignment-1/Solution.java
@@ -1,5 +1,10 @@
 
 import java.util.Scanner;
+/**
+ * @brief [brief description]
+ * @details [long description]
+ * @return [description]
+ */
 final class Solution {
 /*
     Fill this main function to print maximum of given array
@@ -11,7 +16,7 @@ final class Solution {
     /**
      * @param args [description]
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int[] arr;
