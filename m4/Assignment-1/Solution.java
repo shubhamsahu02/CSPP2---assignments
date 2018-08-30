382a3771e72823bf8b0f2f7bdfb6a09fea127764
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-30 20:06:19 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-1/Solution.java
+++ m4/Assignment-1/Solution.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
  * @return [description]
  */
 final class Solution {
-/*
-    Fill this main function to print maximum of given array
-    */
+    /*
+        Fill this main function to print maximum of given array
+        */
     /**
      * this is a class.
      */
@@ -26,20 +26,20 @@ final class Solution {
         }
         int temp = 0;
         for (int i = 0; i < n; i++) {
-         if (i < n - 1) {
-            if (arr[i] > arr[i + 1]) {
-                if (arr[i] > temp) {
-                    temp = arr[i];
+            if (i < n - 1) {
+                if (arr[i] > arr[i + 1]) {
+                    if (arr[i] > temp) {
+                        temp = arr[i];
+                    }
                 }
             }
+            if (arr[n - 1] > temp) {
+                temp = arr[n - 1];
+            } else {
+                temp = temp;
+            }
         }
-        if (arr[n - 1] > temp) {
-            temp = arr[n - 1];
-        } else {
-            temp = temp;
-        }
-    }
-    System.out.println(temp);
+        System.out.println(temp);
 
     }
 }
