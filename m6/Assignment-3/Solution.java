27942bde1fda85ad40cf87c539b7282d34774e33
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-09-01 12:07:23 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m6/Assignment-3/Solution.java
+++ m6/Assignment-3/Solution.java
@@ -25,14 +25,14 @@ final class Solution {
      */
     static char[] replaceAll(final char[] charArray,
         final char oldChar, final char newChar) {
-	// write your code here
+    // write your code here
         for (int i = 0; i < charArray.length; i++) {
-            if(charArray[i] == oldChar) {
+            if (charArray[i] == oldChar) {
                 charArray[i] = newChar;
             }
         }
             return charArray;
-        
+
     }
     /**
      * Main function.
