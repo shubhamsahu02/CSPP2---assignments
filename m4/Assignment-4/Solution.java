d75300d22e650311dd5cebf8005a765a7ec529c3
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-09-02 19:41:13 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m4/Assignment-4/Solution.java
+++ m4/Assignment-4/Solution.java
@@ -5,23 +5,22 @@
 import java.util.Scanner;
 public class Solution
 {/*
-	Do not modify the main function 
-	*/
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		String s=sc.nextLine();
-		String reverse=reverseString(s);	
-		System.out.println(reverse);
-		
-	}
-	//Write reverseString function
+    Do not modify the main function 
+    */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);
+        System.out.println(reverse);
+        
+    }
+    //Write reverseString function
 
 public static String reverseString(String s) {
-	String reverse = "";
-	for (int i = s.length()-1; i >=0; i--) {
-		reverse = reverse + s.charAt(i);
-	}
-	return reverse;
-	}
+    String reverse = "";
+    for (int i = s.length()-1; i >=0; i--) {
+        reverse = reverse + s.charAt(i);
+    }
+    return reverse;
+    }
 }
