90a8596586bd987f5a771b27c2931dc8a7ddc21e
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-09-02 23:39:53 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- m4/Assignment-3/Solution.java
+++ m4/Assignment-3/Solution.java
@@ -3,29 +3,30 @@
 
 
 import java.util.Scanner;
-public class Solution
-{/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
-		for(int i=0;i<=n;i++){
-			String s=sc.next();
-			int res=binaryToDecimal(s);  //Write binaryToDecimal function
-			System.out.println(res);
-		}
-	}
 
-		public static int binaryToDecimal (String s) {
-			double res = 0;
-			for(int i = 0; i < s.length(); i++) {
-				if(s.charAt(i) == 'i') {
-					res = res + Math.pow(2, s.length()-1-i);
-				}
-			}
-			return (int)res;
-		}	
-	}
+public class Solution {
+   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            int res = binaryToDecimal(s); //Write binaryToDecimal function
+            System.out.println(res);
+        }
+    }
+ 
+    public static int binaryToDecimal(String s) {
+        double res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+            res = res + Math.pow(2, s.length()-1-i);
+
+            }
+        }
+        return (int)res;
+
+    }
+
+}
 
