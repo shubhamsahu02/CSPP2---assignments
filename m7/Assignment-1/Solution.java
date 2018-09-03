25d90b711f30b21175f2fb69470b3e1d49c1d27c
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-09-03 23:11:18 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m7/Assignment-1/Solution.java
+++ m7/Assignment-1/Solution.java
@@ -1,49 +1,49 @@
 import java.util.Scanner;
 class InputValidator {
-	/**.
-	 * @brief [brief description]
-	 * @details [long description]
-	 * @param strCheck1 [description]
-	 * @return [description]
-	 */
-	/*Write the atrributes and methods for InputValidator*/
-	private String strCheck;
-	private int len;
-	/**.
-	 * @brief [brief description]
-	 * @details [long description]
-	 * @param strCheck1 [description]
-	 */
-	InputValidator(String strCheck1) {
-		strCheck = strCheck1;
-		len = strCheck1.length();
-	}
+    /**.
+     * @brief [brief description]
+     * @details [long description]
+     * @param strCheck1 [description]
+     * @return [description]
+     */
+    /*Write the atrributes and methods for InputValidator*/
+    private String strCheck;
+    private int len;
+    /**.
+     * @brief [brief description]
+     * @details [long description]
+     * @param strCheck1 [description]
+     */
+    InputValidator(String strCheck1) {
+        strCheck = strCheck1;
+        len = strCheck1.length();
+    }
 /**.
  * @brief [brief description]
  * @details [long description]
  * @return [description]
  */
-	public boolean validateData() {
-		final int b = 6;
-		if(len >= b) {
-			return true;
-		}
-		return false;
-	}
+    public boolean validateData() {
+        final int b = 6;
+        if(len >= b) {
+            return true;
+        }
+        return false;
+    }
 
 }
 public class Solution {
 
-	public static void main(final String[] args) {
-		/**
-		 * @brief [brief description]
-		 * @details [long description]
-		 * @param  [description]
-		 */
-    	Scanner s = new Scanner(System.in);
-    	String input = s.next();
-    	InputValidator i = new InputValidator(input);
-    	System.out.println(i.validateData());
+    public static void main(final String[] args) {
+        /**
+         * @brief [brief description]
+         * @details [long description]
+         * @param  [description]
+         */
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);
+        System.out.println(i.validateData());
 
     }
 
