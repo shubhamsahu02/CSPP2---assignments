50c645c937d4093972347b4d304f27a5a50fe61c
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-09-03 23:02:02 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m7/Assignment-1/Solution.java
+++ m7/Assignment-1/Solution.java
@@ -1,13 +1,37 @@
-import java.util.*;
+import java.util.Scanner;
+/**
+ * @brief [brief description]
+ * @details [long description]
+ * 
+ * @param strCheck1 [description]
+ * @return [description]
+ */
 class InputValidator {
+	/**
+	 * @brief [brief description]
+	 * @details [long description]
+	 * 
+	 * @param strCheck1 [description]
+	 * @return [description]
+	 */
 	/*Write the atrributes and methods for InputValidator*/
 	private String strCheck;
 	private int len;
+	/**
+	 * @brief [brief description]
+	 * @details [long description]
+	 * 
+	 * @param strCheck1 [description]
+	 */
 	InputValidator(String strCheck1) {
 		strCheck = strCheck1;
 		len = strCheck1.length();
 	}
-
+/**
+ * @brief [brief description]
+ * @details [long description]
+ * @return [description]
+ */
 	public boolean validateData() {
 		final int b = 6;
 		if(len >= b) {
@@ -17,8 +41,21 @@ class InputValidator {
 	}
 
 }
+/**
+ * @brief [brief description]
+ * @details [long description]
+ * 
+ * @param args [description]
+ */
 public class Solution {
 	public static void main(String args[]) {
+		/**
+		 * @brief [brief description]
+		 * @details [long description]
+		 * 
+		 * @param  [description]
+		 * @return [description]
+		 */
     	Scanner s = new Scanner(System.in);
     	String input = s.next();
     	InputValidator i = new InputValidator(input);    	
