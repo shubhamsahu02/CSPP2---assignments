752bd54e4fd820989ee9906c4412b3f35642642d
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-09-03 22:58:53 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m7/Assignment-1/Solution.java
+++ m7/Assignment-1/Solution.java
@@ -22,9 +22,9 @@ public class Solution
 {
 	public static void main(String args[])
     {
-    	Scanner s=new Scanner(System.in);
-    	String input=s.next();
-    	InputValidator i=new InputValidator(input);    	
+    	Scanner s = new Scanner(System.in);
+    	String input = s.next();
+    	InputValidator i = new InputValidator(input);    	
     	System.out.println(i.validateData());
 
     }
