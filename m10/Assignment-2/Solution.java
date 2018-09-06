313d5c95fa0b38e1683e4211496bfaaa2ce0eb04
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-09-06 19:52:11 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- m10/Assignment-2/Solution.java
+++ m10/Assignment-2/Solution.java
@@ -8,14 +8,14 @@ final class Solution {
 
 /**
 This is a comment.
- */   
+ */
     private Solution() {
 
     }
     /**
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         // create an object of the list to invoke methods on it
         StringList sl = new StringList();
         StringListInterface l = (StringListInterface) sl;
