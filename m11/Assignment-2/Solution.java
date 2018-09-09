0fffd87f3142130c98c12191bb8c5486d47cd5e9
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-09-09 19:28:17 +0530

    "check_style_score -> 8 of 8 passed. style score: 0/1 "

--- m11/Assignment-2/Solution.java
+++ m11/Assignment-2/Solution.java
@@ -522,6 +522,9 @@ public final class Solution {
     }
   }
 
+  /**
+   * @param      args  The arguments
+   */
   public static void main(final String[] args) {
     // create an object of the list to invoke methods on it
     Scanner stdin = new Scanner(new BufferedInputStream(System.in));
