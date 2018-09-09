de9c1bb0410aa1522150d0f9543c3c29af978dae
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-09-09 19:34:36 +0530

    "check_style_score -> 8 of 8 passed. style score: 0/1 "

--- m11/Assignment-2/Solution.java
+++ m11/Assignment-2/Solution.java
@@ -14,9 +14,6 @@ class Student {
 
   /**
    * @brief [brief description]
-   * @details [long description]
-   *
-   * @param String [description]
    */
   Student(final String nm) {
     //A constructor used to initialize the instance variables
