b27755144c2b132bb83d4316ee3afc4e46c6c792
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-09-11 23:28:08 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- m14/SortedSet/Solution.java
+++ m14/SortedSet/Solution.java
@@ -46,12 +46,12 @@ class Sortedset extends Set {
     }
     public void sort(final int[] arr) {
         int temp;
-        for (int i=0;i<size;i++) {
-            for (int j=i+1; j<size;j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
                 if (arr[i] > arr[j]) {
-                    temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
