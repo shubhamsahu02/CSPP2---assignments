649e8d7493b47e1754f13b342d6a9bd2d4313d0b
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2019-06-18 00:10:05 +0530

    "m4_a4: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- m4/Assignment-4/Solution.java
+++ m4/Assignment-4/Solution.java
@@ -1,26 +1,45 @@
+
+//Write a java method that returns the decimal value for the binary string.
+
+
 import java.util.Scanner;
-
-public class Solution {
-
-
-    Solution() {}
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine();
-        String reverse = reverse(s);
+/**
+ * @return [description]
+ */
+public final class Solution {
+/*
+    Do not modify the main function
+    */
+    /**.
+     *[description]
+     */
+    private Solution() { }
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     *
+     * @param args [description]
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);
         System.out.println(reverse);
     }
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     *
+     * @param s [description]
+     * @return [description]
+     */
 
-
-    public static String reverse(String s) {
-        String reverse = "";
-        for(int i = s.length() - 1; i >= 0; i--) {
-            reverse += s.charAt(i);
-        }
-
-        return reverse;
+public static String reverseString(final String s) {
+    String reverse = "";
+    //This block of code does the reverse for a given string
+    for (int i = s.length() - 1; i >= 0; i--) {
+        reverse = reverse + s.charAt(i);
     }
-
+    return reverse;
+    }
 }
-
